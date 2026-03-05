3ebe55392268570ff859d20e3dfc4d8465765992
lavanyasunkara <sunkara260498@gmail.com>
2026-03-04 21:26:27 -0500

    0004-MEDIAN-OF-TWO-SORTED-ARRAYS Stats: Time: 0 ms (100%), Space: 12.5 MB (81.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -5,11 +5,7 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        list1=[]
-        for i in nums1:
-            list1.append(i)
-        for i in nums2:
-            list1.append(i)
+        list1=nums1+nums2
         list1.sort()
         n=len(list1)
         return list1[n/2] if n%2!=0 else float(float((list1[n/2])+float(list1[(n/2)-1]))/2)
